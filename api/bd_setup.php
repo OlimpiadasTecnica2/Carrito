c43cd93880211f883a3baeac9c5d32f7bec3bff6
Gonanf <gabysolotoresvky100@gmail.com>
2025-06-10 09:00:57 -0300

    db tools

---
+++ api/bd_setup.php
@@ -0,0 +1,25 @@
+<?php 
+
+try {
+	$db = new \PDO("sqlite:base.db");
+	$db -> exec("CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, titular TEXT, email TEXT, contraseña TEXT, numero_tarjeta INTEGER, fecha_vencimiento INTEGER, cvv INTEGER, direccion TEXT);");
+	$db -> exec("CREATE TABLE IF NOT EXISTS productos (id INTEGER PRIMARY KEY, nombre TEXT, imagen TEXT, precio NUMBER);");
+	$db -> exec("CREATE TABLE IF NOT EXISTS carrito (id_usuario INTEGER, id_producto INTEGER, cantidad INTEGER);");
+
+	if ($_SERVER['REQUEST_METHOD'] == "POST"){
+		$_POST = json_decode(file_get_contents('php://input'),true);
+
+		$nombre = $_POST['nombre'];
+		$imagen = $_POST['imagen'];
+		$precio = $_POST['precio'];
+
+		$db -> prepare("INSERT INTO productos(nombre,imagen,precio) VALUES(:nombre,:imagen,:precio);");
+		$db -> execute([':nombre' : $nombre, ':imagen' : $imagen, ':precio' : $precio]);
+	}
+
+} catch (\PDOException $e){
+	echo e-> getMessage();
+
+}
+
+?>
